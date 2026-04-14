059d2ce514be2fcf619c83ce881f591c42615963
jimvak <73341186+jimvak@users.noreply.github.com>
2026-04-14 10:59:01 +0300

    Update stroke_rf_drop_bmi.py

--- src/stroke_rf_drop_bmi.py
+++ src/stroke_rf_drop_bmi.py
@@ -43,7 +43,7 @@ X['smoking_status']= le.fit_transform(X['smoking_status'])
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25)
 
 
-#creating the model
+#creating the RandomForestClassifier model
 clf=RandomForestClassifier(n_estimators=5000)
 
 #training
